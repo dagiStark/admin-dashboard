32f6c2c90a7318a07c9edb53b9e6e53bf64d4415
dagiStark <160214504+dagiStark@users.noreply.github.com>
2024-08-24 11:11:14 +0300

    completed create property functionality

--- client/src/components/common/Form.tsx
+++ client/src/components/common/Form.tsx
@@ -1,13 +1,16 @@
 import {
   Box,
+  Button,
   FormControl,
   FormHelperText,
+  MenuItem,
   Select,
   Stack,
   TextareaAutosize,
   TextField,
   Typography,
 } from "@mui/material";
+import { CustomButton } from "./CustomButton";
 
 export const Form = ({
   type,
@@ -95,18 +98,100 @@ export const Form = ({
                 Select property type
               </FormHelperText>
               <Select
-              variant="outlined"
-              color="info"
-              displayEmpty
-              required
-              inputProps={{'aria-label': 'Without label'}}
-              defaultValue="aparment"
-              {...register('propertyType', {required: true})}
+                variant="outlined"
+                color="info"
+                displayEmpty
+                required
+                inputProps={{ "aria-label": "Without label" }}
+                defaultValue="aparment"
+                {...register("propertyType", { required: true })}
               >
-
+                <MenuItem value="apartment">apartment</MenuItem>
+                <MenuItem value="villa">villa</MenuItem>
+                <MenuItem value="farmhouse">farmhouse</MenuItem>
+                <MenuItem value="condos">condos</MenuItem>
+                <MenuItem value="townhouse">townhouse</MenuItem>
+                <MenuItem value="duplex">duplex</MenuItem>
+                <MenuItem value="studio">studio</MenuItem>
+                <MenuItem value="chalet">chalet</MenuItem>
               </Select>
             </FormControl>
+            <FormControl>
+              <FormHelperText
+                sx={{
+                  fontWeight: 500,
+                  margin: "10px 0",
+                  fontSize: 16,
+                  color: "#11142d",
+                }}
+              >
+                Enter property price
+              </FormHelperText>
+              <TextField
+                fullWidth
+                required
+                id="outlined-basic"
+                type="number"
+                color="info"
+                variant="outlined"
+                {...register("price", { required: true })}
+              />
+            </FormControl>
           </Stack>
+          <FormControl>
+              <FormHelperText
+                sx={{
+                  fontWeight: 500,
+                  margin: "10px 0",
+                  fontSize: 16,
+                  color: "#11142d",
+                }}
+              >
+                Enter location
+              </FormHelperText>
+              <TextField
+                fullWidth
+                required
+                id="outlined-basic"
+                color="info"
+                variant="outlined"
+                {...register("location", { required: true })}
+              />
+            </FormControl>
+            <Stack direction="column" gap={1} justifyContent="center" mb={2}>
+              <Stack direction="row" gap={2}>
+                <Typography color="#11142d" fontSize={16} fontWeight={500} my="10px">
+                  Property photo
+                </Typography>
+                <Button component="label" sx={{
+                  width: 'fit-content',
+                  color: '#2ed480',
+                  textTransform: 'capitalize',
+                  fontSize: 16
+                }}>
+                  upload *
+                  <input 
+                  hidden
+                  accept="image/*"
+                  type="file"
+                  onChange={(e)=>{
+                    // @ts-ignore
+                    handleImageChange(e.target.files[0])
+                  }}
+
+                  />
+                </Button>
+              </Stack>
+              <Typography fontSize={14} color="#808191" sx={{wordBreak: 'break-all'}}>
+                {propertyImage?.name}
+              </Typography>
+            </Stack>
+            <CustomButton 
+            type="submit"
+            title={formLoading ? 'submitting...' : 'submit'}
+            backgroundColor="#475be8"
+            color="#fcfcfc"
+            />
         </form>
       </Box>
     </Box>
--- client/src/pages/create-properties.tsx
+++ client/src/pages/create-properties.tsx
@@ -2,28 +2,48 @@ import React, { useState } from "react";
 import { useGetIdentity } from "@refinedev/core";
 import { useForm } from "@refinedev/react-hook-form";
 import { Form } from "../components";
+import { FieldValues } from "react-hook-form";
 
 export const CreateProperties = () => {
-  const { data: user } = useGetIdentity();
+  const { data: user }: any = useGetIdentity();
   const [propertyImage, setPropertyImage] = useState({
     name: "",
     url: "",
   });
-  const {refineCore: {onFinish, formLoading}, register, handleSubmit} = useForm();
-  const handleImageChange = () =>{}
+  const {
+    refineCore: { onFinish, formLoading },
+    register,
+    handleSubmit,
+  } = useForm();
 
+  const handleImageChange = (file: File) => {
+    const reader = (readFile: File) =>
+      new Promise<string>((resolve, reject) => {
+        const fileReader = new FileReader();
+        fileReader.onload = () => resolve(fileReader.result as string);
+        fileReader.readAsDataURL(readFile);
+      });
 
-  return(
-    <Form 
-    type="Create"
-    register={register}
-    onFinish={onFinish}
-    formLoading={formLoading}
-    handleSubmit={handleSubmit}
-    handleImageChange={handleImageChange}
-    onFinishHandler={onFinish}
-    propertyImage={propertyImage}
+    reader(file).then((result: string) =>
+      setPropertyImage({ name: file?.name, url: result })
+    );
+  };
+
+  const onFinishHandler = async (data: FieldValues) => {
+    if (!propertyImage.name) return alert("select image first");
+    await onFinish({ ...data, photo: propertyImage.url, email: user.email });
+  };
+
+  return (
+    <Form
+      type="Create"
+      register={register}
+      onFinish={onFinish}
+      formLoading={formLoading}
+      handleSubmit={handleSubmit}
+      handleImageChange={handleImageChange}
+      onFinishHandler={onFinishHandler}
+      propertyImage={propertyImage}
     />
-  )
-  ;
+  );
 };
